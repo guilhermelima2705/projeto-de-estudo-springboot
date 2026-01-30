3a79a750b3a603a034779d1fd07202a5e3ae7b21
Guilherme de Souza Lima <guilhermelima.ocn@gmail.com>
2026-01-30 09:34:18 -0300

    Projeto inicial Spring Boot

---
+++ src/test/java/com/guilherme/aulaspring/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.aulaspring.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
