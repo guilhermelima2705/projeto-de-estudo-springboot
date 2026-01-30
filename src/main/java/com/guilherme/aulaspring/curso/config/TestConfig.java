6b4b60b9d474a1a8d1a07a4946741b0d7d48d78c
Guilherme de Souza Lima <guilhermelima.ocn@gmail.com>
2026-01-30 13:27:23 -0300

    Implementando o Service layer e o component registration

--- src/main/java/com/guilherme/aulaspring/curso/config/TestConfig.java
+++ src/main/java/com/guilherme/aulaspring/curso/config/TestConfig.java
@@ -15,7 +15,6 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
-
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
