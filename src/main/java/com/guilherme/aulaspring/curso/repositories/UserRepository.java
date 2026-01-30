5fc34c467ccb5ef6cbfc41e7df045e8d2b987cb8
Guilherme de Souza Lima <guilhermelima.ocn@gmail.com>
2026-01-30 11:28:13 -0300

    Implementando o JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/guilherme/aulaspring/curso/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.guilherme.aulaspring.curso.repositories;
+
+import com.guilherme.aulaspring.curso.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
