ea483d9e75672bb1b4c7620a94dd53b7b6bd1ad6
Guilherme de Souza Lima <guilhermelima.ocn@gmail.com>
2026-01-31 18:56:19 -0300

    Tratamento de exceção delete

--- src/main/java/com/guilherme/aulaspring/curso/services/UserService.java
+++ src/main/java/com/guilherme/aulaspring/curso/services/UserService.java
@@ -2,8 +2,11 @@ package com.guilherme.aulaspring.curso.services;
 
 import com.guilherme.aulaspring.curso.entities.User;
 import com.guilherme.aulaspring.curso.repositories.UserRepository;
+import com.guilherme.aulaspring.curso.services.exceptions.DatabaseException;
 import com.guilherme.aulaspring.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
