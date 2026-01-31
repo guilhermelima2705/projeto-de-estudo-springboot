c85218c723e58ae4369223643649fb4e1289c71e
Guilherme de Souza Lima <guilhermelima.ocn@gmail.com>
2026-01-31 10:20:45 -0300

    Criando o tipo enum OrderStatus

--- src/main/java/com/guilherme/aulaspring/curso/entities/Order.java
+++ src/main/java/com/guilherme/aulaspring/curso/entities/Order.java
@@ -1,6 +1,7 @@
 package com.guilherme.aulaspring.curso.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.guilherme.aulaspring.curso.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,6 +19,8 @@ private static final long serialVersionUID = 1L;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -25,10 +28,11 @@ private static final long serialVersionUID = 1L;
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +50,15 @@ private static final long serialVersionUID = 1L;
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+        this.orderStatus = orderStatus.getCode();}
     }
 
     public User getClient() {
